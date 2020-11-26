cfdc43c62ffcee90e5d30fc0712b18df66c6b5b9
igor <igorvinogradov87@gmail.com>
2020-11-26 15:51:37 +0200

    all merged

--- src/App.vue
+++ src/App.vue
@@ -3,40 +3,28 @@
     <v-app-bar
         app
         flat
-
     >
-
-      <!--        <v-app-bar-nav-icon></v-app-bar-nav-icon>-->
-
       <Header/>
       <v-btn icon>
         <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
-
     </v-app-bar>
-
     <router-view/>
-
-
-
     <v-footer>
       <Footer/>
     </v-footer>
   </v-app>
 </template>
-
 <script>
 import Header from '@/components/common/Header';
 import Footer from "@/components/common/Footer";
 
 export default {
   name: 'App',
-
   components: {
     Header,
     Footer
   },
-
   data: () => ({
     //
   }),
--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,46 +1,25 @@
 <template>
-
   <v-content>
     <Hero/>
-    <v-row class="mb-2" no-gutters>
-      <v-col md="2">
-        <v-card class="pa-8" l flat
-        >
-          <v-toolbar
-              dense
-              floating
-          >
-            <v-text-field
-                prepend-icon="mdi-magnify"
-                single-line
-            ></v-text-field>
+
+
+    <v-row class="block latestPostBlock ">
+
+      <v-col md="2" class="pa-2 ">
+
+          <v-toolbar dense floating>
+            <v-text-field prepend-icon="mdi-magnify" single-line></v-text-field>
           </v-toolbar>
-          <v-card
-              outlined
-              elevation="12"
-              width="276"
-          >
-            <v-navigation-drawer
-                floating
-                permanent
-            >
+          <v-card outlined elevation="12" width="276">
+            <v-navigation-drawer floating permanent>
               <v-list-item>
                 <v-list-item-content>
-                  <v-list-item-title align="center">
-                    PRODUCTS
-                  </v-list-item-title>
+                  <v-list-item-title align="center">PRODUCTS</v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
               <v-divider></v-divider>
-              <v-list
-                  dense
-                  rounded
-              >
-                <v-list-item
-                    v-for="item in items"
-                    :key="item.title"
-                    link
-                >
+              <v-list dense rounded>
+                <v-list-item v-for="item in items" :key="item.title" link>
                   <v-list-item-icon>
                     <v-icon>{{ item.icon }}</v-icon>
                   </v-list-item-icon>
@@ -51,66 +30,38 @@
               </v-list>
             </v-navigation-drawer>
           </v-card>
-        </v-card>
+
       </v-col>
-      <v-col cols="2" v-for="item in items" :key="item.id">
-        <v-card outlined
-                :loading="loading"
-                class="mx-auto my-12"
-        >
-          <template slot="progress">
-            <v-progress-linear
-                color="deep-purple"
-                height="10"
-                indeterminate
-            ></v-progress-linear>
-          </template>
-          <v-img
-              height="100"
-              v-bind:src="item.src"
-          ></v-img>
-          <v-card-title>{{ item.title }}</v-card-title>
-          <v-card-text>
-            <v-row
-                align="center"
-                class="mx-0"
-            >
-              <v-rating
-                  :value="4.5"
-                  color="amber"
-                  dense
-                  half-increments
-                  readonly
-                  size="14"
-              ></v-rating>
-              <div class="grey--text ml-4">
-                4.5 (413)
-              </div>
-            </v-row>
-            <div class="my-4 subtitle-1">
-              {{ item.location }}
-            </div>
-            <div>{{ item.description }}</div>
-          </v-card-text>
-          <v-divider class="mx-4"></v-divider>
-          <v-card-actions>
-            <v-btn
-                color="deep-purple lighten-2"
-                text
-                @click="reserve"
-            >
-              Reserve
-            </v-btn>
-          </v-card-actions>
-        </v-card>
+      <v-col md="10" class="pa-10  ">
+        <v-row class="block latestPostBlock">
+
+          <v-col md="2" class="ml-auto" v-for="item in items" :key="item.id">
+            <v-card outlined :loading="loading" class="mx-auto my-12">
+              <template slot="progress">
+                <v-progress-linear color="deep-purple" height="10" indeterminate></v-progress-linear>
+              </template>
+              <v-img height="100" v-bind:src="item.src"></v-img>
+              <v-card-title>{{ item.title }}</v-card-title>
+              <v-card-text>
+                <v-row align="center" class="mx-0">
+                  <v-rating :value="4.5" color="amber" dense half-increments readonly size="14"></v-rating>
+                  <div class="grey--text ml-4">4.5 (413)</div>
+                </v-row>
+                <div class="my-4 subtitle-1">{{ item.location }}</div>
+                <div>{{ item.description }}</div>
+              </v-card-text>
+              <v-divider class="mx-4"></v-divider>
+              <v-card-actions>
+                <v-btn color="deep-purple lighten-2" text @click="reserve">Reserve</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-col>
+      <v-spacer></v-spacer>
     </v-row>
     <div class="text-center">
-      <v-pagination
-          v-model="page"
-          :length="15"
-          :total-visible="7"
-      ></v-pagination>
+      <v-pagination v-model="page" :length="15" :total-visible="7"></v-pagination>
     </div>
   </v-content>
 </template>
