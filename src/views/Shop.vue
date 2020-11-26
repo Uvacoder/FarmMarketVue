6eb428bff2b9442cf3713c064016bba4e19a2428
igor <igorvinogradov87@gmail.com>
2020-11-26 10:05:15 +0200

    updated

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -2,64 +2,56 @@
 
   <v-content>
     <Hero/>
-    <v-row>
-      <v-col cols="2"  >
-
-      <v-card
-        class="pa-4"
-        l
-        flat
-    >
-      <v-toolbar
-          dense
-          floating
-          >
-        <v-text-field
-            prepend-icon="mdi-magnify"
-            single-line
-        ></v-text-field>
-      </v-toolbar>
-      <v-card
-          outlined
-          elevation="12"
-          width="276"
-      >
-        <v-navigation-drawer
-            floating
-            permanent
+    <v-row class="mb-2" no-gutters>
+      <v-col md="2">
+        <v-card class="pa-8" l flat
         >
-
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title align="center">
-                PRODUCTS
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-divider></v-divider>
-          <v-list
+          <v-toolbar
               dense
-              rounded
+              floating
           >
-            <v-list-item
-                v-for="item in items"
-                :key="item.title"
-                link
+            <v-text-field
+                prepend-icon="mdi-magnify"
+                single-line
+            ></v-text-field>
+          </v-toolbar>
+          <v-card
+              outlined
+              elevation="12"
+              width="276"
+          >
+            <v-navigation-drawer
+                floating
+                permanent
             >
-              <v-list-item-icon>
-                <v-icon>{{ item.icon }}</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-
-          </v-list>
-        </v-navigation-drawer>
-
-      </v-card>
-
-    </v-card>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title align="center">
+                    PRODUCTS
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-divider></v-divider>
+              <v-list
+                  dense
+                  rounded
+              >
+                <v-list-item
+                    v-for="item in items"
+                    :key="item.title"
+                    link
+                >
+                  <v-list-item-icon>
+                    <v-icon>{{ item.icon }}</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-navigation-drawer>
+          </v-card>
+        </v-card>
       </v-col>
       <v-col cols="2" v-for="item in items" :key="item.id">
         <v-card outlined
@@ -73,14 +65,11 @@
                 indeterminate
             ></v-progress-linear>
           </template>
-
           <v-img
               height="100"
               v-bind:src="item.src"
           ></v-img>
-
           <v-card-title>{{ item.title }}</v-card-title>
-
           <v-card-text>
             <v-row
                 align="center"
@@ -94,22 +83,16 @@
                   readonly
                   size="14"
               ></v-rating>
-
               <div class="grey--text ml-4">
                 4.5 (413)
               </div>
             </v-row>
-
             <div class="my-4 subtitle-1">
-              {{item.location}}
+              {{ item.location }}
             </div>
-
-            <div>{{item.description}}</div>
+            <div>{{ item.description }}</div>
           </v-card-text>
-
           <v-divider class="mx-4"></v-divider>
-
-
           <v-card-actions>
             <v-btn
                 color="deep-purple lighten-2"
@@ -122,8 +105,6 @@
         </v-card>
       </v-col>
     </v-row>
-
-
     <div class="text-center">
       <v-pagination
           v-model="page"
@@ -131,9 +112,7 @@
           :total-visible="7"
       ></v-pagination>
     </div>
-
   </v-content>
-
 </template>
 <script>
 import Hero from "@/components/Hero";
