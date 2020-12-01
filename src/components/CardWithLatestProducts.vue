cde78a58eda4065bd868445e1f6e56c82402a8b5
asun1k <igorvinogradov87@gmail.com>
2020-12-02 00:06:58 +0200

    cards design redone

---
+++ src/components/CardWithLatestProducts.vue
@@ -0,0 +1,63 @@
+<template>
+
+  <v-row>
+
+    <div class="v-main__wrap">
+      <div class="container">
+        <div class="row ">
+          <v-col cols="4" v-for="item in products" :key="item.id">
+            <div class="card">
+              <v-card >
+                <v-img height="100" v-bind:src="item.src"></v-img>
+                <v-card-title class="card-title">{{ item.productName }}</v-card-title>
+                <v-card-text class="cardText">
+                  <div class="my-0">
+                    <div>{{ item.productDescription }}</div>
+                  </div>
+                </v-card-text>
+                <div class="price">$ {{ item.price }}</div>
+                <v-divider class="mx-4"></v-divider>
+                <v-card-actions>
+                  <v-btn text class="button">BUY</v-btn>
+                  <v-spacer></v-spacer>
+                  <v-btn icon>
+                    <v-icon>mdi-heart</v-icon>
+                  </v-btn>
+                  <v-btn icon>
+                    <v-icon>mdi-bookmark</v-icon>
+                  </v-btn>
+                  <v-btn icon>
+                    <v-icon>mdi-share-variant</v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </div>
+          </v-col>
+        </div>
+      </div>
+    </div>
+
+  </v-row>
+</template>
+<script>
+
+let getLatestProducts = function () {
+  let url = "http://localhost:8090/getLatestProducts";
+  this.$http.get(url)
+      .then(response => this.products = response.data)
+}
+export default {
+  name: 'CardWithLatestProducts',
+  data: () => ({
+    products: [],
+    productId: 0,
+  }),
+  methods: {
+    getLatestProductsFunc: getLatestProducts,
+  },
+  created: function () {
+    this.getLatestProductsFunc()
+  }
+
+}
+</script>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,21 +1,35 @@
 <template>
-    <v-main>
-      <Hero/>
-      <v-toolbar-title class="subHeader text-center">MAIN</v-toolbar-title>
-      <LatestPost/>
-    </v-main>
+  <v-main>
+    <Hero/>
+    <v-toolbar-title class="subHeader text-center">latest products</v-toolbar-title>
+    <div class="mainBlock">
+      <v-container>
+        <v-row>
+          <div class="col-3">
+            <Menu/>
+          </div>
+          <div class="col-8">
+            <CardWithLatestProducts/>
+          </div>
+        </v-row>
+      </v-container>
+    </div>
+
+  </v-main>
 </template>
 
 <script>
 import Hero from "@/components/Hero";
-import LatestPost from "@/components/LatestPost";
+import CardWithLatestProducts from "@/components/CardWithLatestProducts";
+import Menu from "@/components/Menu";
 
   export default {
     name: 'Main',
 
     components: {
-      LatestPost,
+      CardWithLatestProducts,
       Hero,
+      Menu,
     },
 
     data () {
