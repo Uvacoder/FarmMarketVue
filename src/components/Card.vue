941907b50054ba83d076e71b4789b0ee5bbd0541
anna lazarenkova <anna.lazarenkova@gmail.com>
2020-12-04 10:51:46 +0200

    R hommik

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -15,19 +15,40 @@
               </v-card-text>
               <div class="price">$ {{ item.price }}</div>
               <v-divider class="mx-4"></v-divider>
+              <v-dialog v-model="dialog" persistent max-width="600px">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn color="primary" dark v-bind="attrs" v-on="on">Contact seller </v-btn>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="headline">Product details</span>
+                  </v-card-title>
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12" sm="6">
+                          <v-autocomplete :items="['Skiing', 'Ice hockey', 'Soccer']" label="Product category" multiple></v-autocomplete>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field label="Product name" aria-required="true">{{item.productName }}</v-text-field></v-col>
+                        <v-col cols="12"><v-text-field label="Product description" required></v-text-field></v-col>
+                        <v-col cols="12" sm="6"><v-text-field label="Price" required></v-text-field></v-col>
+                        <v-col cols="12" sm="6"><v-text-field label="Amount" required></v-text-field></v-col>
+                      </v-row>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" @click="dialog=false">Close</v-btn>
+                        <v-btn color="blue darken-1" @click="contactSeller()">Send email to seller</v-btn>
+                      </v-card-actions>
+                    </v-container>
+                  </v-card-text>
+                </v-card>
+              </v-dialog>
               <v-card-actions>
-                <v-btn text class="button">BUY</v-btn>
                 <v-spacer></v-spacer>
-                <v-btn icon>
-                  <v-icon>mdi-heart</v-icon>
-                </v-btn>
-                <v-btn icon>
-                  <v-icon>mdi-bookmark</v-icon>
-                </v-btn>
-                <v-btn icon>
-                  <v-icon>mdi-share-variant</v-icon>
-                </v-btn>
-
+                <v-btn icon><v-icon>mdi-heart</v-icon></v-btn>
+                <v-btn icon><v-icon>mdi-bookmark</v-icon></v-btn>
+                <v-btn icon><v-icon>mdi-share-variant</v-icon></v-btn>
               </v-card-actions>
             </v-card>
             </div>
@@ -58,17 +79,25 @@ let getProducts = function (){
     this.getLatestProducts()
   }
 }
+
+let contactSellerFunc = function (){
+
+}
+
+
 export default {
   name: 'Card',
   data: () => ({
     products: [],
     productId: 0,
   }),
+  dialog: false,
   props: {
     query: String
   },
   methods: {
     getProductsFunc: getProducts,
+    contactSeller: contactSellerFunc(),
     getAllProducts,
     getLatestProducts
   },
--- src/views/EditAccount.vue
+++ src/views/EditAccount.vue
@@ -106,126 +106,69 @@
 
             <v-row no-gutters>
               <v-col md="3">
-                <div class="blue-grey--text pa-5">
-                  Username:
-                </div>
+                <div class="blue-grey--text pa-5">Username:</div>
               </v-col>
               <v-col md="7">
-                <v-text-field
-                    label="Username"
-                    single-line
-                    v-model="seller.username"
-                ></v-text-field>
+                <v-text-field label="Username" single-line v-model="seller.username"></v-text-field>
               </v-col>
               <v-col md="2">
                 <div class="pa-3">
-                  <v-btn
-                      text
-                      medium
-                      color="primary"
-                  >
-                    UPDATE
-                  </v-btn>
+                  <v-btn text medium color="primary">UPDATE</v-btn>
                 </div>
               </v-col>
             </v-row>
             <v-row no-gutters>
               <v-col md="3">
-                <div class="blue-grey--text pa-5">
-                  Password:
-                </div>
+                <div class="blue-grey--text pa-5">Password:</div>
               </v-col>
               <v-col md="7">
-                <v-text-field
-                    label="Password"
-                    single-line
-                    v-model="seller.password"
-                ></v-text-field>
+                <v-text-field label="Password" single-line v-model="seller.password"></v-text-field>
               </v-col>
               <v-col md="2">
                 <div class="pa-3">
-                  <v-btn
-                      text
-                      medium
-                      color="primary"
-                  >
-                    UPDATE
-                  </v-btn>
+                  <v-btn text medium color="primary">UPDATE</v-btn>
                 </div>
               </v-col>
             </v-row>
             <v-row no-gutters>
               <v-col md="3">
-                <div class="blue-grey--text pa-5">
-                  Personal information:
-                </div>
+                <div class="blue-grey--text pa-5">Personal information:</div>
               </v-col>
               <v-col md="7">
                 <v-text-field
                     label="Personal Information"
                     single-line
-                    v-model="seller.personalInformation"
-                ></v-text-field>
+                    v-model="seller.personalInformation"></v-text-field>
               </v-col>
               <v-col md="2">
                 <div class="pa-3">
-                  <v-btn
-                      text
-                      medium
-                      color="primary"
-                  >
-                    UPDATE
-                  </v-btn>
+                  <v-btn text medium color="primary">UPDATE</v-btn>
                 </div>
               </v-col>
             </v-row>
             <v-row no-gutters>
               <v-col md="3">
-                <div class="blue-grey--text pa-5">
-                  Address:
-                </div>
+                <div class="blue-grey--text pa-5">Address:</div>
               </v-col>
               <v-col md="7">
-                <v-text-field
-                    label="Address"
-                    single-line
-                    v-model="seller.address"
-                ></v-text-field>
+                <v-text-field label="Address" single-line v-model="seller.address"></v-text-field>
               </v-col>
               <v-col md="2">
                 <div class="pa-3">
-                  <v-btn
-                      text
-                      medium
-                      color="primary"
-                  >
-                    UPDATE
-                  </v-btn>
+                  <v-btn text medium color="primary">UPDATE</v-btn>
                 </div>
               </v-col>
             </v-row>
             <v-row no-gutters>
               <v-col md="3">
-                <div class="blue-grey--text pa-5">
-                  Phone:
-                </div>
+                <div class="blue-grey--text pa-5">Phone:</div>
               </v-col>
               <v-col md="7">
-                <v-text-field
-                    label="Phone"
-                    single-line
-                    v-model="seller.phone"
-                ></v-text-field>
+                <v-text-field label="Phone" single-line v-model="seller.phone"></v-text-field>
               </v-col>
               <v-col md="2">
                 <div class="pa-3">
-                  <v-btn
-                      text
-                      medium
-                      color="primary"
-                  >
-                    UPDATE
-                  </v-btn>
+                  <v-btn text medium color="primary">UPDATE</v-btn>
                 </div>
               </v-col>
             </v-row>
